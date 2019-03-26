d0c1658e89f73ce2b7266d5ddea9d83da5062e0c
Lukas <lukas.hilfrich@mni.thm.de>
2019-03-26 18:43:39 +0100

    prettify uzi hit explosion animation

--- src/models/animations/UziHitExplosionAnimation.java
+++ src/models/animations/UziHitExplosionAnimation.java
@@ -12,13 +12,13 @@ public class UziHitExplosionAnimation extends AbstractAnimation {
 
     @Override
     public void addNewInstance() throws SlickException {
-        Image smoke_animation_image = new Image("assets/animations/bullet_explosion.png");
+        Image smoke_animation_image = new Image("assets/animations/bullet_explosion2.png");
         Animation explosion_animation = new Animation(false);
         int IMAGE_COUNT = 9;
         int x = 0;
         for (int idx = 0; idx < IMAGE_COUNT; ++idx) {
-            explosion_animation.addFrame(smoke_animation_image.getSubImage(x, 0, 15, 19), 80);
-            x += 15;
+            explosion_animation.addFrame(smoke_animation_image.getSubImage(x, 0, 11, 28), 80);
+            x += 11;
         }
         explosion_animation.setLooping(false);
         buffered_instances.add(new AnimationInstance(explosion_animation));
