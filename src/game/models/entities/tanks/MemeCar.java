2473fc11a209c02ae99d246e56295c35698bb916
Lukas <lukas.hilfrich@mni.thm.de>
2020-12-26 07:14:29 +0100

    adjust meme car values

--- src/game/models/entities/tanks/MemeCar.java
+++ src/game/models/entities/tanks/MemeCar.java
@@ -1,5 +1,6 @@
 package game.models.entities.tanks;
 
+import game.models.weapons.Uzi;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
@@ -10,14 +11,16 @@ public class MemeCar extends Tank {
     private static Texture meme_car_texture, meme_car_turret_texture;
 
     // attributes
-    private static final float ARMOR = 20.f;
+    private static final float ARMOR = 10.f;
     private static final int SCORE_VALUE = 50;
     private static final float ROTATE_SPEED_PLAYER = 0.15f, ROTATE_SPEED_BOT = 0.15f;
-    private static final float MAX_SPEED_PLAYER = 0.1f, MAX_SPEED_BOT = 0.07f;
-    private static final float ACCELERATION_FACTOR = 0.0005f, DECELERATION_FACTOR = 0.0005f;
+    private static final float MAX_SPEED_PLAYER = 0.3f, MAX_SPEED_BOT = 0.2f;
+    private static final float ACCELERATION_FACTOR = 0.0005f, DECELERATION_FACTOR = 0.0003f;
 
     public MemeCar(Vector2f startPos, boolean isHostile, boolean isDrivable) {
         super(startPos, isHostile, isDrivable);
+
+        weapons.add(new Uzi(isDrivable));  // WEAPON_1
 
         // LOAD TEXTURES
         try {
