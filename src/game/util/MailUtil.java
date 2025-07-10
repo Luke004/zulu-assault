13097810efe42bd2210a4a95e0ed545d9d702e95
Lukas H <l.hilfrich@gmx.de>
2025-07-11 00:56:48 +0200

    make mail feedback work again

--- src/game/util/MailUtil.java
+++ src/game/util/MailUtil.java
@@ -6,20 +6,13 @@ import java.util.Properties;
 
 public class MailUtil {
 
+    private static final String FEEDBACK_SENDER = "f.zuluassault@web.de";
+    private static final String FEEDBACK_RECEIVER = "l.hilfrich@gmx.de";
+
     public static void sendFeedbackMail(String senderName, String senderMail, String senderMsg)
             throws Exception {
-        final String sender = "lh-funmail3@web.de";
-        final String receiver = "l.hilfrich@gmx.de";
 
-        Properties properties = new Properties();
-        properties.put("mail.transport.protocol", "smtp");
-        properties.put("mail.smtp.host", "smtp.web.de");
-        properties.put("mail.smtp.port", "587");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.user", sender);
-        properties.put("mail.smtp.password", "9%uk/S6-BZhC3s_");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.ssl.trust", "smtp.web.de");
+        Properties properties = getProperties();
 
         Session mailSession = Session.getInstance(properties, new Authenticator() {
             @Override
@@ -30,12 +23,24 @@ public class MailUtil {
         });
 
         Message message = new MimeMessage(mailSession);
-        InternetAddress addressTo = new InternetAddress(receiver);
+        InternetAddress addressTo = new InternetAddress(FEEDBACK_RECEIVER);
         message.setRecipient(Message.RecipientType.TO, addressTo);
-        message.setFrom(new InternetAddress(sender));
+        message.setFrom(new InternetAddress(FEEDBACK_SENDER));
         message.setSubject("New feedback from " + (senderName.isEmpty() ? "Anonymous" : senderName) + "!");
         message.setContent(senderMsg + "\n\nSender Mail: " + senderMail, "text/plain");
         Transport.send(message);
     }
 
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.host", "smtp.web.de");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.user", FEEDBACK_SENDER);
+        properties.put("mail.smtp.password", "vE9ZZ':C2C7*vqM");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.ssl.trust", "smtp.web.de");
+        return properties;
+    }
 }
